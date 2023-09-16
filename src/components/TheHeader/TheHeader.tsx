320a4a10301fece505e8d1fa615f777ce2ace72a
Volodymyr Herman <vova.german1789@gmail.com>
2023-09-17 00:43:25 +0300

    next js expose

---
+++ src/components/TheHeader/TheHeader.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './header.module.css';
+
+export default function TheHeader() {
+  return (
+    <header className={styles.Header}>TheHeader</header>
+  )
+}
